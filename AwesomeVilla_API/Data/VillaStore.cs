5438fe51690a45158fb7bce4c8635ee7b6f39760
habiboshodorimu@gmail.com <habiboshodorimu@gmail.com>
2024-07-15 09:26:40 +0100

    Add project files.

---
+++ AwesomeVilla_API/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeVilla_API.Models.DTO;
+
+namespace AwesomeVilla_API.Data
+{
+    public class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+           {
+               new VillaDTO{Id = 1, Name = "Pool View"},
+               new VillaDTO{Id = 2, Name = "Beach View"}
+           };
+    }
+}
